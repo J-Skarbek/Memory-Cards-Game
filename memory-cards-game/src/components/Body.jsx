e6891394b46b9e08a6eab5d546eedc30720ab4e5
J-Skarbek <john@snarkmediaagency.com>
2023-04-20 18:32:27 -0500

    Add basic cards to a State array as objects

--- memory-cards-game/src/components/Body.jsx
+++ memory-cards-game/src/components/Body.jsx
@@ -2,6 +2,46 @@ import React from "react";
 
 function Body() {
 
+  const [cards, setCards] = React.useState([
+    {
+      name: 'card 1',
+    },
+    {
+      name: 'card 2'
+    },
+    {
+      name: 'card 3'
+    },
+    {
+      name: 'card 4'
+    },
+    {
+      name: 'card 5',
+    },
+    {
+      name: 'card 6'
+    },
+    {
+      name: 'card 7'
+    },
+    {
+      name: 'card 8'
+    },
+    {
+      name: 'card 9',
+    },
+    {
+      name: 'card 10'
+    },
+    {
+      name: 'card 11'
+    },
+    {
+      name: 'card 12'
+    }
+  ])
+
+  console.log(cards)
 
   return (
     <div>
