9b85936a08c28da0be7d0f29bb4b175b3111e937
J-Skarbek <john@snarkmediaagency.com>
2023-04-20 19:00:02 -0500

    Move the fisher-yates into a setState function

--- memory-cards-game/src/components/Body.jsx
+++ memory-cards-game/src/components/Body.jsx
@@ -41,22 +41,43 @@ function Body() {
     }
   ]);
 
-  function shuffle(array) {
-    let m = array.length, t, i;
+  // function shuffle(array) {
+  //   let m = array.length, t, i;
   
-    // While there remain elements to shuffle…
-    while (m) {
+  //   // While there remain elements to shuffle…
+  //   while (m) {
   
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * m--);
+  //     // Pick a remaining element…
+  //     i = Math.floor(Math.random() * m--);
   
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
-    }
-    console.log(array);
-    return setCards(array);
+  //     // And swap it with the current element.
+  //     t = array[m];
+  //     array[m] = array[i];
+  //     array[i] = t;
+  //   }
+  //   console.log(array);
+  //   return setCards(array);
+  // }
+
+  function shuffleCards(array) {
+    // let newArr = array;
+    setCards(newArr => {
+      let m = newArr.length, t, i;
+  
+      // While there remain elements to shuffle…
+      while (m) {
+    
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+    
+        // And swap it with the current element.
+        t = newArr[m];
+        newArr[m] = newArr[i];
+        newArr[i] = t;
+      }
+      return newArr;
+    })
+    // console.log(newArr)
   }
 
 
@@ -68,7 +89,7 @@ function Body() {
   return (
     <div>
       <p>Test</p>
-      <button onClick={() => shuffle(cards)}>Click to test shuffle the cards array</button>
+      <button onClick={() => shuffleCards(cards)}>Click to test shuffle the cards array</button>
       <button onClick={testState}>Click to see the state values</button>
   
     </div>
