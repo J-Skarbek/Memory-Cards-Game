a17103b850fab7383bc0c37dad3e1400f02d9a2b
J-Skarbek <john@snarkmediaagency.com>
2023-04-23 11:48:13 -0500

    Create header component and remove boilerplate from app.jsx

--- memory-cards-game/src/components/Header.jsx
+++ memory-cards-game/src/components/Header.jsx
@@ -1,1 +1,22 @@
 import React from "react";
+
+function Header(props) {
+
+
+  return (
+    <div className="header flex">
+      <div className="header-right">
+        <h1>Memory Card Game</h1>
+        <p>Get points by clicking on an image but don't click on
+          any image more than once!
+        </p>
+      </div>
+      <div className="header-left">
+        <p>Score: 0</p>
+        <p>Best Score: 12</p>
+     </div>
+    </div>
+  )
+}
+
+export default Header;
