8937bcb98553f4e116a4fa4a09a2544bc2e0a964
J-Skarbek <john@snarkmediaagency.com>
2023-04-23 12:16:06 -0500

    Add tailwind classes to style the header

--- memory-cards-game/src/components/Header.jsx
+++ memory-cards-game/src/components/Header.jsx
@@ -4,14 +4,14 @@ function Header(props) {
 
 
   return (
-    <div className="header flex bg-offWhite">
-      <div className="header-right">
+    <div className="header flex justify-between items-center bg-offWhite p-4 mb-8">
+      <div className="header-right flex flex-col items-start">
         <h1>Memory Card Game</h1>
         <p>Get points by clicking on an image but don't click on
           any image more than once!
         </p>
       </div>
-      <div className="header-left">
+      <div className="header-left flex flex-col items-end">
         <p>Score: 0</p>
         <p>Best Score: 12</p>
      </div>
